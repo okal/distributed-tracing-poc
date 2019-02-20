93791fc5cfd1e5ac1f0ca871c5e282a904241057
Okal Otieno <okal@justokal.com>
2019-02-20 15:22:19 +0300

    Add zipkin_span decorator to get_results view function

--- frontend/results/views.py
+++ frontend/results/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from py_zipkin.zipkin import zipkin_span
 
 
+@zipkin_span(
+    host="zipkin",
+    span_name="get_results",
+    service_name="frontend",
+    transport_handler=None
+)
 @api_view(['get'])
 def get_results(_):
     return Response({'result': 42})
-

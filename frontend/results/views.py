af306f177e778f3b353d8527d7a86d2903ce3661
Okal Otieno <okal@justokal.com>
2019-02-21 12:10:45 +0300

    Switch to context manager from decorator to provide access to trace_id

--- frontend/results/views.py
+++ frontend/results/views.py
@@ -1,16 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from py_zipkin.zipkin import zipkin_span, ZipkinAttrs
+from py_zipkin.zipkin import zipkin_span
 from tracing.transport import SimpleHTTPTransportHandler
 
-@zipkin_span(
-    host="zipkin",
-    port=9411,
-    span_name="get_results",
-    service_name="frontend",
-    transport_handler=SimpleHTTPTransportHandler(),
-    sample_rate=100.0
-)
+
+
 @api_view(['get'])
 def get_results(_):
-    return Response({'result': 42})
+    with zipkin_span(
+        host="zipkin",
+        port=9411,
+        span_name="get_results",
+        service_name="frontend",
+        transport_handler=SimpleHTTPTransportHandler(),
+        sample_rate=100.0,
+    ):
+        return Response({'result': 42})

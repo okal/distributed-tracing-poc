03ad83325702cfc90161b46b8939af72520cf0a0
Okal Otieno <okal@justokal.com>
2019-02-21 11:48:24 +0300

    Use simple HTTP transport class in decorator

--- frontend/results/views.py
+++ frontend/results/views.py
@@ -1,13 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from py_zipkin.zipkin import zipkin_span
-
+from py_zipkin.zipkin import zipkin_span, ZipkinAttrs
+from tracing.transport import SimpleHTTPTransportHandler
 
 @zipkin_span(
     host="zipkin",
+    port=9411,
     span_name="get_results",
     service_name="frontend",
-    transport_handler=None
+    transport_handler=SimpleHTTPTransportHandler(),
+    sample_rate=100.0
 )
 @api_view(['get'])
 def get_results(_):
